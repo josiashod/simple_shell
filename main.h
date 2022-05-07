73cb0326ddbec6e3917078d0e13662c42aca58c4
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-07 04:34:10 -0700

    Add file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: returns EXIT_FAILURE OF EXIT_SUCCESS
+ *
+ */
+int main(int argc, char **argv, char **env)
+{
+}
