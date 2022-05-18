cf384f41838382020278ad76794f924965204e70
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-18 15:48:07 -0700

    try

--- README.md
+++
@@ -1,2 +0,0 @@
-# simple_shell
-Write a simple UNIX command interpreter in C
--- errors.h
+++
@@ -1,7 +0,0 @@
-#ifndef ERRORS_H
-#define ERRORS_H
-
-char *FORK_FAILED = "Fork failed";
-char *INVALID_COMMAND = "Invalid command";
-
-#endif
